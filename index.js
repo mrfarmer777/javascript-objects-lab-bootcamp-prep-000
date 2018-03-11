8b5b1cd2a81d6cd1a994789075babb99fcad72c7
mrfarmer777@gmail.com <mrfarmer777@gmail.com>
2018-03-11 20:38:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,20 @@ var recipes ={};
 var updateObjectWithKeyAndValue=function(object,key,value){
   Object.assign({},object,{[key]:value});
   return object;
+};
+
+var destructivelyUpdateObjectWithKeyAndValue=function(object,key,value){
+  object[key]=value;
+  return object;
+};
+
+var deleteFromObjectByKey=function(object,key){
+  var clone = Object.assign({},obj);
+  delete clone[key];
+  return clone;
 }
 
+var destructivelyDeleteFromObjectByKey=function(object,key){
+  delete object[key];
+  return object;
+}
