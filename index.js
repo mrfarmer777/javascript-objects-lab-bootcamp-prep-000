ba4e5dfd4ae6050c2b2121d8df42df2b4ef2c067
mrfarmer777@gmail.com <mrfarmer777@gmail.com>
2018-03-11 20:39:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var destructivelyUpdateObjectWithKeyAndValue=function(object,key,value){
 };
 
 var deleteFromObjectByKey=function(object,key){
-  var clone = Object.assign({},obj);
+  var clone = Object.assign({},object);
   delete clone[key];
   return clone;
 }
